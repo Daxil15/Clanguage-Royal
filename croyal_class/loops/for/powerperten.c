244859cf225c383518c6c1af1aaf33309d799604
Daxil15 <chakranidaxil@gmail.com>
2026-08-27 11:06:07 +0530

    28-8-2026

---
+++ croyal_class/loops/for/powerperten.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void main(){
+    int i,j,k, ans = 1;
+
+    for(i=0;i<5;i++)
+    {
+        // for(j=0;j<5-i;j++)
+        // {
+        //     printf(" ");
+        // }//space loop end
+        ans = ans * 11;
+        for(k=0;k=1;k++)
+        {
+            if(k==1)
+            {
+                printf("%d ",ans);
+            }
+        }//print loop end
+        printf("\n");
+    }// outer loop end
+}
